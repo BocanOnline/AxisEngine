8a5bc2163a8897deb26c838f1020bd998a886c26
buchananmatt <buch.matt@gmail.com>
2026-03-29 19:07:01 -0600

    (maint) updated build scripts to build/

--- scripts/run-debug.sh
+++ scripts/run-debug.sh
@@ -10,19 +10,19 @@ app="${1:-}"
 
 case "$app" in
     laser)
-        exec "./build-debug/source/AxisEngine-App-Laser/AxisEngine-App-Laser"     "${@:2}"
+        exec "./build/debug/source/AxisEngine-App-Laser/AxisEngine-App-Laser"     "${@:2}"
         ;;
     picker)
-        exec "./build-debug/source/AxisEngine-App-Picker/AxisEngine-App-Picker"   "${@:2}"
+        exec "./build/debug/source/AxisEngine-App-Picker/AxisEngine-App-Picker"   "${@:2}"
         ;;
     plotter)
-        exec "./build-debug/source/AxisEngine-App-Plotter/AxisEngine-App-Plotter" "${@:2}"
+        exec "./build/debug/source/AxisEngine-App-Plotter/AxisEngine-App-Plotter" "${@:2}"
         ;;
     printer)
-        exec "./build-debug/source/AxisEngine-App-Printer/AxisEngine-App-Printer" "${@:2}"
+        exec "./build/debug/source/AxisEngine-App-Printer/AxisEngine-App-Printer" "${@:2}"
         ;;
     router)
-        exec "./build-debug/source/AxisEngine-App-Router/AxisEngine-App-Router"   "${@:2}"
+        exec "./build/debug/source/AxisEngine-App-Router/AxisEngine-App-Router"   "${@:2}"
         ;;
     "")
         echo "Usage: $0 <app> [args...]"
